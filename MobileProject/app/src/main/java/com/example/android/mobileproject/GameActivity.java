9867f86599163eec6026f62036732789024f58cb
jrunzer26 <jason.runzer@uoit.net>
2016-10-24 20:34:18 -0400

    map now follows user

--- MobileProject/app/src/main/java/com/example/android/mobileproject/GameActivity.java
+++ MobileProject/app/src/main/java/com/example/android/mobileproject/GameActivity.java
@@ -5,6 +5,11 @@ import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -12,21 +17,37 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+//import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnCameraIdleListener;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+
 
 import android.support.v7.app.AppCompatActivity;
 
-public class GameActivity extends FragmentActivity implements OnCameraIdleListener, OnMapReadyCallback {
+import java.security.Provider;
+
+public class GameActivity extends FragmentActivity implements LocationSource, OnCameraIdleListener, LocationListener, OnMapReadyCallback {
 
     private GoogleMap mMap;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private static final float MIN_ZOOM_PREF = 18f;
+    private static final float MAX_ZOOM_PREF = MIN_ZOOM_PREF;
+    private LocationManager locationManager;
+    private final int minTime = 20000; // milliseconds
+    private final int minDistance = 3; // meters
+    private String bestProvider;
+    private OnLocationChangedListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +57,23 @@ public class GameActivity extends FragmentActivity implements OnCameraIdleListen
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        // set the criteria of the provider
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_HIGH);
+        criteria.setAltitudeRequired(false);
+        criteria.setPowerRequirement(Criteria.NO_REQUIREMENT);
+        criteria.setSpeedRequired(true);
+        criteria.setCostAllowed(true);
+        // find the best location manager that meets the criteria
+        bestProvider = locationManager.getBestProvider(criteria, true);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+            return;
+        }
+        // add the location manager
+        locationManager.requestLocationUpdates(bestProvider, minTime, minDistance, this);
     }
 
 
@@ -51,8 +89,12 @@ public class GameActivity extends FragmentActivity implements OnCameraIdleListen
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+
         // set the listeners
         mMap.setOnCameraIdleListener(this);
+        mMap.setMinZoomPreference(MIN_ZOOM_PREF);
+        mMap.setMaxZoomPreference(MAX_ZOOM_PREF);
         // Add a marker in Sydney and move the camera
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // get the location permissions
@@ -60,6 +102,12 @@ public class GameActivity extends FragmentActivity implements OnCameraIdleListen
             return;
         }
         mMap.setMyLocationEnabled(true);
+
+        // get and animate to user location
+        double lat = locationManager.getLastKnownLocation(bestProvider).getLatitude();
+        double lng = locationManager.getLastKnownLocation(bestProvider).getLatitude();
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(lat, lng)));
+
     }
 
     @Override
@@ -82,5 +130,44 @@ public class GameActivity extends FragmentActivity implements OnCameraIdleListen
         SharedPreferences sharedPreferences = getSharedPreferences(LoginActivity.SHAREDPREF_USERINFO, Context.MODE_PRIVATE);
         sharedPreferences.edit().clear().commit();
         finish();
+    }
+
+    /**
+     * Called whent he location of the user changes
+     * @param location
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        System.out.println("location changed");
+        // added to ensure that the map is animated
+        if (listener != null) {
+            listener.onLocationChanged(location);
+        }
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        listener = onLocationChangedListener;
+    }
+
+    @Override
+    public void deactivate() {
+
     }
 }
