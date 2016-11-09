cc6d671fbcd57f18b2a76fbc45685d4b5fab95e1
jrunzer26 <jason.runzer@uoit.net>
2016-11-08 22:44:04 -0500

    now accesses the webservice for tile information, and can captures users location tile

---
+++ TerritoryWarGameMap/app/src/main/java/com/mobileproject/game/TileWebserviceUtility.java
@@ -0,0 +1,56 @@
+package com.mobileproject.game;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by 100520993 on 11/8/2016.
+ */
+
+/**
+ * Used to capture a tile or get its resources/create it.
+ */
+public class TileWebserviceUtility {
+
+    public static void captureTile(int tileLatID, int tileLngID, String username, String password, AsyncResponse callback, Context context) {
+        JSONObject jsonObject = new JSONObject();
+        JSONObject auth = new JSONObject();
+        try {
+            auth.put("username", username);
+            auth.put("password", password);
+            jsonObject.put("username", username);
+            jsonObject.put("tileLatID", tileLatID);
+            jsonObject.put("tileLngID", tileLngID);
+            System.out.println(jsonObject.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ServerControl capture = new ServerControl(callback);
+        capture.execute(context.getString(R.string.server) + "tiles/capture",
+                ServerControl.POST, jsonObject.toString(), auth.toString());
+    }
+
+    public static void getResources(int tileLatID, int tileLngID, String username, String password, AsyncResponse callback, Context context) {
+        JSONObject jsonObject = new JSONObject();
+        JSONObject auth = new JSONObject();
+        try {
+            auth.put("username", username);
+            auth.put("password", password);
+            jsonObject.put("tileLatID", tileLatID);
+            jsonObject.put("tileLngID", tileLngID);
+            System.out.println(jsonObject.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ServerControl sc = new ServerControl(callback);
+        sc.execute(context.getString(R.string.server) + "tiles/resources",
+                ServerControl.POST, jsonObject.toString(), auth.toString());
+    }
+
+
+}
